bd4ad709565b06589bf835d468a1c179a6514a8c
CHICH <charbelh162@gmail.com>
2025-07-11 03:52:55 -0400

    package set up

---
+++ python_dsa/gsl_331/__init__.py
@@ -0,0 +1,2 @@
+from .core import Queue, Stack, Graph, Node,LLNode,LinkedList,Priority_queue
+__version__ = "1.0.0"
---
+++ python_dsa/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gsl_331",
+    version="1.0.0",
+    author="CHICH",
+    description="Custom data structures library",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
