17f230e01ee5fdbbbec310bf9a6c455d54faebb0
CHICH <charbelh162@gmail.com>
2025-07-11 04:03:53 -0400

    fixing bugs

--- python_dsa/gsl_331/__init__.py
+++ python_dsa/gsl_331/__init__.py
@@ -1,2 +1,39 @@
-from .core import Queue, Stack, Graph, Node,LLNode,LinkedList,Priority_queue
+"""
+GSL-331 Data Structures Library
+
+A comprehensive Python library providing custom implementations of essential data structures
+and algorithms including queues, stacks, priority queues, graphs, linked lists, and more.
+
+Author: CHICH
+"""
+
+from .core import (
+    Queue,
+    Stack,
+    Priority_queue,
+    Node,
+    LLNode,
+    LinkedList,
+    Graph,
+    BFS_response,
+    DFS_response,
+    Edges_type,
+    less_important
+)
+
 __version__ = "1.0.0"
+__author__ = "CHICH"
+
+__all__ = [
+    'Queue',
+    'Stack', 
+    'Priority_queue',
+    'Node',
+    'LLNode',
+    'LinkedList',
+    'Graph',
+    'BFS_response',
+    'DFS_response',
+    'Edges_type',
+    'less_important'
+]
--- python_dsa/setup.py
+++ python_dsa/setup.py
@@ -1,16 +1,36 @@
 from setuptools import setup, find_packages
 
+# Read the contents of README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="gsl_331",
+    name="gsl-331",
     version="1.0.0",
     author="CHICH",
-    description="Custom data structures library",
+    author_email="cbe05@mail.aub.edu",  
+    description="A comprehensive Python library for data structures and algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    python_requires=">=3.6",
-    install_requires=[],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Education",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.7",
+    install_requires=[
+        # No external dependencies - uses only Python standard library
+    ],
+    keywords="data-structures algorithms queue stack graph priority-queue linked-list bfs dfs eece 331",
 )
